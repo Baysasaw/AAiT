f349e0bc25797408163055649e60301f9fd7ea41
baysasaw <yonas988252656@gmail.com>
2023-05-05 15:35:47 +0300

    programming

--- RollDie.java
+++ RollDie.java
@@ -9,29 +9,31 @@ public class RollDie{
         int frequency4 = 0;
         int frequency5 = 0;
         int frequency6 = 0;
-        for (int i = 0; i <= 600_000_000; i++){
+        for (int i = 0; i <= 600_000; i++){
             int face = 1 + randomNumber.nextInt(6);
-            Switch(face){
+            switch (face){
                 case 1:
-                    frequency1++;
+                    ++frequency1;
                     break;
-                case 2:
-                    frequency2++;
-                    break;
-                case 3:
-                    frequency3++;
-                    break;
-                case 4:
-                    frequency4++;
-                    break;
-                case 5:
-                    frequency5++;
-                    break;
-                case 6:
-                    frequency6++;
-                    break;
+            case 2:
+                frequency2++;
+                break;
+            case 3:
+                frequency3++;
+                break;
+            case 4:
+                frequency4++;
+                break;
+            case 5:
+                frequency5++;
+                break;
+            case 6:
+                frequency6++;
+                break;
             }
         }
+        System.out.printf("Face\tFrequency%n 1\t%d%n 2\t%d%n 3\t%d%n 4\t%d%n 5\t%d%n 6\t%d%n"
+        ,frequency1, frequency2, frequency3, frequency4, frequency5, frequency6 );
 
     }
 }
